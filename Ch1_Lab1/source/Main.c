5e772fe3258d403ca0dceed5ef300840b6333ade
str001030011 <1030011@gmail.com>
2020-09-25 21:24:45 +0800

    first

---
+++ Ch1_Lab1/source/Main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	printf("Wwlcome to C!\n");
+	system("PAUSE");
+	return 0;
+}
---
+++ Ch1_Lab2/source/Main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "User_defined.h"
+
+int main(void)
+{
+	User_defined_print();
+
+	system("PAUSE");
+	return 0;
+}
